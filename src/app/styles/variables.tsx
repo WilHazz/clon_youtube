7eb9f9826931999b2f391bac0cc41bbec19fbefe
Hanny Paz <hwpazz@gmail.com>
2025-09-25 12:28:50 -0500

    agregando variables para los botones

---
+++ src/app/styles/variables.tsx
@@ -0,0 +1,17 @@
+// importamos desde React-Iconos
+import { FaBars, FaSearch, FaBell, FaUserCircle } from "react-icons/fa";
+import { MdVideoLibrary, MdHome, MdSubscriptions } from "react-icons/md";
+import { AiFillLike, AiFillDislike } from "react-icons/ai";
+
+// Exportamos como objeto para mantener orden
+export const Icons = {
+    menu: FaBars,
+    search: FaSearch,
+    bell: FaBell,
+    user: FaUserCircle,
+    home: MdHome,
+    library: MdVideoLibrary,
+    subscriptions: MdSubscriptions,
+    like: AiFillLike,
+    dislike: AiFillDislike,
+}
