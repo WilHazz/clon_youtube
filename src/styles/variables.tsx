2d101239fa019c5a697bea54c6e119f295c0345d
Hanny Paz <hwpazz@gmail.com>
2025-09-26 11:16:53 -0500

    agregando cambios en variables.tsx

--- src/styles/variables.tsx
+++ src/styles/variables.tsx
@@ -3,6 +3,7 @@ import { FaBars, FaSearch, FaBell, FaUserCircle } from "react-icons/fa";
 import { MdVideoLibrary, MdHome, MdSubscriptions } from "react-icons/md";
 import { AiFillLike, AiFillDislike } from "react-icons/ai";
 
+
 // Exportamos como objeto para mantener orden
 export const Icons = {
     menu: FaBars,
