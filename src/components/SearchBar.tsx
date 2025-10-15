b1e74fd517c417b66aebd6cbc99e85891fef18f7
Hanny Paz <hwpazz@gmail.com>
2025-10-15 10:33:29 -0500

    agregando un nuevo componente para el busador en modo mobile

---
+++ src/components/SearchBar.tsx
@@ -0,0 +1,11 @@
+"use client";
+import { useTheme } from "next-themes"; 
+import {  useState } from "react";
+import { Icons } from "@/styles/variables";
+
+
+export default function SearchBar (){
+  return (
+    <div>SearchBar</div>
+  )
+}
