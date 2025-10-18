a8ada6abd8d2b661089d3f0201bd9f6593e0e266
Hanny Paz <hwpazz@gmail.com>
2025-10-18 12:29:33 -0500

    agregando libreria clsx

---
+++ src/components/Sidebar.tsx
@@ -0,0 +1,9 @@
+"use client";
+import { useState } from "react";
+import { Icons } from "@/styles/variables";
+import clsx from "clsx";
+
+export default function Sidebar(){
+    const [isOpen, setIsOpen] = useState(false);
+}
+
