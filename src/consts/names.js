3ae901cffdb2927aa311bdd1f93d6aea750a3fa8
Hanny Paz <hwpazz@gmail.com>
2026-06-03 12:11:15 -0500

    names para yt

--- src/consts/names.js
+++ src/consts/names.js
@@ -1,3 +1,3 @@
 export const names = ["John", "Jane", "Bob", "Alice", 
     "Pedro", "Marcos", "Fabian", "Florentino", "Cesar", 
-    "Diego", "Ginna", "Jaime", "Albert", "Juanes", "Maria", "Alana", "Wil Wilson"];
+    "Diego", "Ginna", "Jaime", "Albert", "Juanes", "Maria", "Alana", "Wil Wilson", "Perez"];
