73fc434fc79b7f39d6181aab6dc0246f0c4e0b15
Hanny Paz <hwpazz@gmail.com>
2026-06-17 11:05:46 -0500

    agregando names para youtube

--- src/consts/names.js
+++ src/consts/names.js
@@ -1,4 +1,5 @@
 export const names = ["John", "Jane", "Bob", "Alice", 
     "Pedro", "Marcos", "Fabian", "Florentino", "Cesar", 
     "Diego", "Ginna", "Jaime", "Albert", "Juanes", "Maria", "Alana", "Wil Wilson", "Perez", 
-    "El militar", "El comandate", "Busquets", "Amigos D", "Kamkam", "Louis"];
+    "El militar", "El comandate", "Busquets", "Amigos D", "Kamkam",
+     "Louis", "Principer de persia"];
