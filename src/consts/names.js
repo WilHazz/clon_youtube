4e70809e63d6c879f8713e7ed11ef7cc6c04ebfb
Hanny Paz <hwpazz@gmail.com>
2026-06-24 10:19:04 -0500

    Agregando nombres para yt

--- src/consts/names.js
+++ src/consts/names.js
@@ -2,4 +2,5 @@ export const names = ["John", "Jane", "Bob", "Alice",
     "Pedro", "Marcos", "Fabian", "Florentino", "Cesar", 
     "Diego", "Ginna", "Jaime", "Albert", "Juanes", "Maria", "Alana", "Wil Wilson", "Perez", 
     "El militar", "El comandate", "Busquets", "Amigos D", "Kamkam",
-     "Louis", "Principe de persia", "Jorgeis", "Roles", "Retos y mas retos"];
+     "Louis", "Principe de persia", "Jorgeis", "Roles", 
+     "Retos y mas retos", "Super tv"];
